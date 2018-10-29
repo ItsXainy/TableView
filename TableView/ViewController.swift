572e52ddaf69ed35b022e5e26138db95a79cb3bc
ZAINAB TAREEN <rl190058@rlas-macair-34.rlas-116.org>
2018-10-29 12:12:46 -0500

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by ZAINAB TAREEN on 10/29/18.
+//  Copyright © 2018 ZAINY TAREEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
